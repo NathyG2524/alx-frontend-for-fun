4b58e73fde173d3d0dd4f5a46566d46453e55725
NathyG2524 <nathygirma2524@gmail.com>
2022-07-03 21:54:39 +0300

    markdown to html

--- markdown2html.py
+++ markdown2html.py
@@ -2,11 +2,14 @@
 import sys
 import os
 
-if(len(sys.argv) < 3):
-    print("Usage: ./markdown2html.py README.md README.html")
+if __name__ == '__main__':
+    if(len(sys.argv) < 3):
+        print("Usage: ./markdown2html.py README.md README.html", file = sys.stderr)
 
 
-markdown = sys.argv[1]
-file_exits = os.path.exists(markdown)
-if not (file_exits):
-    print(f"Missing {markdown}")
+    markdown = sys.argv[1]
+    file_exits = os.path.exists(markdown)
+    if not (file_exits):
+        print(f"Missing {markdown}", file = sys.stderr)
+
+
